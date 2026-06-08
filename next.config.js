728c182e79de961db30f96b76171232f88d6c030
miccowang66 <miccowang66@example.com>
2026-06-08 16:39:50 +0800

    Configure static export and GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  basePath: "/HW6_machine-learning-all-in-one",
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
